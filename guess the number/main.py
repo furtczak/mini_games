5f69ae22c8f43ac29b68787f9beaed79793aa1b9
MrRazeLL <frytka202020@gmail.com>
2022-12-07 02:41:04 +0100

    test

--- guess the number/main.py
+++
@@ -1,8 +0,0 @@
-from guessthenumber import  guess_the_numer
-while True:
-    txt = """Mini Game!!!
-    pres 1 to play guess the number """
-    num = input(txt)
-    if num =="1":
-        guess_the_numer(20)
-    
