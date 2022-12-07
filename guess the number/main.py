3aa38388ca0af0d95e6b209806912b50a22ef4ce
MrRazeLL <frytka202020@gmail.com>
2022-12-07 02:27:50 +0100

    first commit

--- guess the number/main.py
+++ guess the number/main.py
@@ -2,6 +2,7 @@ from guessthenumber import  guess_the_numer
 while True:
     txt = """Mini Game!!!
     pres 1 to play guess the number """
-    value= input(txt)
-    if value =="1":
-        pass 
+    num = input(txt)
+    if num =="1":
+        guess_the_numer(100)
+    
