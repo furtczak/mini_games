0b34de6c27010d49b74733813634b74b33592d6f
MrRazeLL <frytka202020@gmail.com>
2022-12-07 02:26:38 +0100

    first commit

---
+++ guess the number/guessthenumber.py
@@ -0,0 +1,18 @@
+import random
+
+def guess_the_numer(x):
+    print("Let's play guess the number")
+
+    random_number = random.randint(0,x)
+
+    num_guesses = 0
+    while True:
+        guess=int(input(f"guess the number in range 0-{x}"))
+        num_guesses +=1
+
+        if guess == random_number:
+            print(f"Congrats, The looking number is {x}")
+        elif guess < random_number:
+            print("Too low")
+        else:
+            print("Too high ")
--- guess the number/main.py
+++ guess the number/main.py
@@ -1,1 +1,7 @@
-print('hello world')
+from guessthenumber import  guess_the_numer
+while True:
+    txt = """Mini Game!!!
+    pres 1 to play guess the number """
+    value= input(txt)
+    if value =="1":
+        pass 
