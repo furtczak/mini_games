14702080d786a119c3f1f8b9aaea8c16573516ef
MrRazeLL <frytka202020@gmail.com>
2022-12-07 02:41:57 +0100

    tak com

---
+++ guess the number/main.py
@@ -0,0 +1,14 @@
+
+from guessthenumber import guess_the_number
+
+while True:
+    txt = """Mini Games!!!
+    - Guess The Number (a)
+    - Rock, Paper, Scissors (2)
+    - Wordle (3)
+    - ConnectFour (4)
+    - Tic Tac Toe (5)
+Select a game (press a number or 'q' to quit): """
+    value = input(txt)
+    if value == "a":
+        guess_the_number(100)
